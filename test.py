74aa9a44fd901e2559e7ee0cc7a0f342069e3fb4
web-code <tchhvorn@gmail.com>
2026-01-25 21:25:22 +0700

    Add files via upload
    Signed-off-by: web-code <tchhvorn@gmail.com>

---
+++ test.py
@@ -0,0 +1,16 @@
+import math 
+from turtle import *
+def heartra(k): 
+    return 15 * math.sin(k) ** 3
+def heartb(k):
+    return 13 * math.cos(k) - 5 *\
+    math.cos(2 * k) - 2 *\
+    math.cos(3 * k) - math.cos(4 * k)
+speed(10000)
+bgcolor("pink")
+for i in range(6000):
+    goto(heartra(i)*20, heartb(i)*20)
+    for j in range(5):
+        color("#f73487")
+    goto(0, 0)
+done()
